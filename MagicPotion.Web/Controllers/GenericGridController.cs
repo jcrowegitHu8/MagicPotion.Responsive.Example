b69737951af9a017da22f21801a30dd5b18bda9a
Jason Crowe <jasoncrowe5+git@gmail.com>
2017-08-31 06:14:41 -0500

    Generic react grid.

---
+++ MagicPotion.Web/Controllers/GenericGridController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MagicPotion.Business;
+using MagicPotion.Web.Models;
+
+namespace MagicPotion.Web.Controllers
+{
+    public class GenericGridController : Controller
+    {
+
+	    private readonly IIngredientManager _ingredientManager;
+
+	    public GenericGridController(IIngredientManager ingredientManager)
+	    {
+		    _ingredientManager = ingredientManager;
+	    }
+
+	    // GET: GenericGridExample
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+	    public ActionResult GetData(int dataId)
+	    {
+		    var ingredients = _ingredientManager.GetAllIngredients();
+			var grid = new GenericReactGrid(new List<string> { "Name", "Address", "Date Issued" }, dataId);
+		    foreach (var rowData in ingredients)
+		    {
+			    grid.GridRow.Add(new Dictionary<string, object>
+			    {
+				    {"col1", rowData.Name},
+				    {"col2", rowData.Description},
+				    {"col3", rowData.Color},
+				    {"id", rowData.Id },
+				    //{"updateUrl", Url.Action()},
+				    //{"deleteUrl", Url.Action("Delete", "") },
+				    //{"deleteParams", new Dictionary<string, object>{{"type", "" }}}
+			    });
+
+		    }
+
+		    return Json(new GenericReactTableResult(grid), JsonRequestBehavior.AllowGet);
+		}
+    }
+}
---
+++ MagicPotion.Web/Models/GenericReactTableResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace MagicPotion.Web.Models
+{
+	
+
+	public class GenericReactTableResult
+	{
+		public GenericReactTableResult(GenericReactGrid grid, int sofaId = 0, string updateContainer = null)
+		{
+			UpdateContainer = updateContainer;
+
+			Grid = new List<GenericReactGrid>();
+			if (grid != null)
+			{
+				Grid.Add(grid);
+			}
+		}
+
+
+		[JsonProperty(PropertyName = "updateContainer")]
+		public string UpdateContainer { get; set; }
+
+		[JsonProperty(PropertyName = "grid")]
+		public List<GenericReactGrid> Grid { get; set; }
+	}
+}
